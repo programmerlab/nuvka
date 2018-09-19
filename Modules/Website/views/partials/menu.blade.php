c59d618b3381ace02181c5423ec98029c1118b97
programmerlab <kroy.iips@gmail.com>
2018-09-19 19:09:03 +0530

    website description

--- Modules/Website/views/partials/menu.blade.php
+++ Modules/Website/views/partials/menu.blade.php
@@ -8,13 +8,13 @@
       <div class="header-top-inner">
        
         
-     <div class="offer-text" >
+     <div class="logo" >
         <a href="{{url('/')}}">  <img src="{{url('storage/uploads/img/'.$setting->website_logo)}}" style="width: 100px; height: 55px;"> </a>
 
         </div>
 
 
-          <div class="col-md-6" style="margin-left: 10px; margin-top: 10px; color: #fff; font-size: 25px; font-family: 'Cinzel', serif !important"> 
+          <div class="col-md-6" style="margin-left: 10px; margin-top: 10px; color: #fff; font-size: 25px; font-family: 'Open Sans', sans-serif; float: left;"> 
                {{$setting->website_title??''}} 
           
         </div>
