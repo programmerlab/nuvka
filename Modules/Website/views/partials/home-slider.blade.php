245f5708de7e3636b9fc0b0e0da4344737b71dca
programmerlab <kroy.iips@gmail.com>
2018-09-30 17:28:26 +0530

    images issue

--- Modules/Website/views/partials/home-slider.blade.php
+++ Modules/Website/views/partials/home-slider.blade.php
@@ -1,16 +1,19 @@
+  @php
+
+  $img1 =  url('storage/uploads/img/'.$setting->banner_image1);
+  $img2 =  url('storage/uploads/img/'.$setting->banner_image2);
+  $img3 =  url('storage/uploads/img/'.$setting->banner_image3);
+
+  @endphp
+
    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
         <div id="hero">
           <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
            
             <!-- /.item -->
           
-          @if(file_exists(storage_path('uploads/img/'.$setting->banner_image1)))
-            <div class="item" style="background-image: url({{ asset('storage/uploads/img/'.$setting->banner_image1)}});">
-              @else
-              <div class="item" style="background-image: url({{ asset('public/enduser/assets/images/sliders/02.jpg')}});">
-              
-              @endif
-
+            <div class="item" style="background-image: url('{{$img1}}');">
+               
               <div class="container-fluid">
                 <div class="caption bg-color vertical-center text-left">
                  
@@ -20,24 +23,16 @@
               <!-- /.container-fluid --> 
             </div>
 
-               @if(file_exists(storage_path('uploads/img/'.$setting->banner_image2)))
-            <div class="item" style="background-image: url({{ asset('storage/uploads/img/'.$setting->banner_image3)}});">
-              @else
-              <div class="item" style="background-image: url({{ asset('public/enduser/assets/images/sliders/01.jpg')}});">
+            <div class="item" style="background-image: url('{{$img2}}');">
               
-              @endif
               <div class="container-fluid">
               
               </div>
               <!-- /.container-fluid --> 
             </div>
             
-             @if(file_exists(storage_path('uploads/img/'.$setting->banner_image3)))
-            <div class="item" style="background-image: url({{ asset('storage/uploads/img/'.$setting->banner_image3)}});">
-              @else
-              <div class="item" style="background-image: url({{ asset('public/enduser/assets/images/sliders/03.jpg')}});">
-              
-              @endif
+            <div class="item" style="background-image: url('{{$img3}}');">
+             
               <div class="container-fluid">
                 <div class="caption bg-color vertical-center text-left">
                  
@@ -45,9 +40,7 @@
                 <!-- /.caption --> 
               </div>
               <!-- /.container-fluid --> 
-            </div> 
-
-
+            </div>  
             
           </div>
           <!-- /.owl-carousel --> 
